5e9ea5169c8cd1903c54128983efb3a72237f825
LU <1150463081@qq.com>
2022-05-27 18:07:02 +0800

    Update：日常提交

---
+++ AOI/Server/AOIServer/AOIServer/ServerRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOIServer
+{
+    public class ServerRoot
+    {
+
+    }
+}
--- AOI/Server/AOIServer/AOIServer/ServerStart.cs
+++ AOI/Server/AOIServer/AOIServer/ServerStart.cs
@@ -2,7 +2,7 @@
 
 namespace AOIServer
 {
-    class Program
+    class ServerStart
     {
         static void Main(string[] args)
         {
